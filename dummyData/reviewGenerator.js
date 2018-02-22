a47e8bd6718f059d04600e5bf879d151dacb349f
Sara Brandes <smbrandes17@gmail.com>
2018-02-22 10:29:38 -0800

    have dummy data functions mostly written, need to research testing and write tests before PR

---
+++ dummyData/reviewGenerator.js
@@ -0,0 +1,43 @@
+const data = require('./reviewGeneratorData.js');
+const users = require('./dummyUsers');
+
+// let reviewIdCounter = 1001;
+
+const getRandomNumber = limit => (
+  Math.floor(Math.random() * limit)
+);
+
+// {
+//   id: number, UNIQUE
+//   restaurant: number (restaurant id number),
+//   userName: string,
+//   userPhoto: string,
+//   userLocation: string,
+//   rating: number (1-5, whole numbers only),
+//   date: string,
+//   reviewBody: string,
+//   useful: number,
+//   funny: number,
+//   cool: number,
+// }
+
+const generateReview = (restaurantObj, numStars, reviewDate) => {
+  const curUser = users[getRandomNumber(users.length)];
+
+  const review = {
+    restaurant: restaurantObj.id,
+    userName: curUser.name,
+    userPhoto: curUser.photo,
+    userLocation: curUser.location,
+    rating: numStars,
+    date: reviewDate,
+    reviewBody: data(restaurantObj.foodType, restaurantObj.title, numStars),
+    useful: getRandomNumber(7),
+    funny: getRandomNumber(7),
+    cool: getRandomNumber(7),
+  };
+
+  return review;
+};
+
+module.exports = generateReview;
