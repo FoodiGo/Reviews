ed8233d8e871e29f3f4fef1ec855eba9953a9e63
Sara Brandes <smbrandes17@gmail.com>
2018-02-23 17:29:08 -0800

    have database schema and functions basics roughly written, tests still pending

--- database/review.js
+++ database/review.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/reviews');
+
 const reviewSchema = mongoose.Schema({
   restaurant: Number,
   userName: String,
   userPhoto: String,
   userLocation: String,
   rating: Number,
-  date: String,
+  date: Date,
   reviewBody: String,
   useful: Number,
   funny: Number,
@@ -15,5 +17,19 @@ const reviewSchema = mongoose.Schema({
 
 const Review = mongoose.model('Review', reviewSchema);
 
+const insertReview = (reviewObject, callback) => {
+  Review.create(reviewObject).exec(callback);
+};
+
+const findReview = (reviewId, callback) => {
+  Review.findById(reviewId).exec(callback);
+};
+
+const findReviewsByRestaurant = (restaurantId, callback) => {
+  Review.find({ restaurant: restaurantId }).sort('-date').exec(callback);
+};
 
 
+module.exports.insertReview = insertReview;
+module.exports.findReview = findReview;
+module.exports.findReviewsByRestaurant = findReviewsByRestaurant;
