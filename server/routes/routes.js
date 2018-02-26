f2e53983ba87e4c9fb6c0af7b9dd0db4e70b6c56
Sara Brandes <smbrandes17@gmail.com>
2018-02-26 11:27:59 -0800

    forgot to take out console.logs

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,17 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/restaurants/:restaurantId/reviews', (req, res) => {
-  console.log('GET');
   res.json(['hello']);
 });
 
 router.post('/restaurants/:restaurantId/reviews', (req, res) => {
-  console.log('POST');
   res.sendStatus(201);
 });
 
 router.put('/restaurants/:restaurantId/reviews/:reviewId', (req, res) => {
-  console.log('PUT');
   res.sendStatus(200);
 });
 
