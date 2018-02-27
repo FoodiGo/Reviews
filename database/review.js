ff688923c164c7be27eea72afcbd7fcfd179d55a
Sara Brandes <smbrandes17@gmail.com>
2018-02-27 11:16:58 -0800

    Have database functions written, seeder function written, mlab database set up, and database testing environment

--- database/review.js
+++ database/review.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/reviews');
-
 const reviewSchema = mongoose.Schema({
   restaurant: Number,
   userName: String,
@@ -18,18 +16,36 @@ const reviewSchema = mongoose.Schema({
 const Review = mongoose.model('Review', reviewSchema);
 
 const insertReview = (reviewObject, callback) => {
-  Review.create(reviewObject).exec(callback);
+  Review.create(reviewObject, (err, review) => {
+    callback(err, review);
+  });
 };
 
 const findReview = (reviewId, callback) => {
   Review.findById(reviewId).exec(callback);
 };
 
+const updateReview = (reviewId, property, value, callback) => {
+  Review.findById(reviewId, (err, review) => {
+    if (err) {
+      callback(err, null);
+    }
+    review[property] = value; // eslint-disable-line
+    review.save(callback);
+  });
+};
+
 const findReviewsByRestaurant = (restaurantId, callback) => {
   Review.find({ restaurant: restaurantId }).sort('-date').exec(callback);
+};
+
+const findHighestRestaurantId = (callback) => {
+  Review.findOne().sort('-restaurant').exec(callback);
 };
 
 
 module.exports.insertReview = insertReview;
 module.exports.findReview = findReview;
+module.exports.updateReview = updateReview;
 module.exports.findReviewsByRestaurant = findReviewsByRestaurant;
+module.exports.findHighestRestaurantId = findHighestRestaurantId;
